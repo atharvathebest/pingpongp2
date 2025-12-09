cc6ff33fa21e0c90f9e419591ad972f662a0cebb
atharvathebest <145860900+atharvathebest@users.noreply.github.com>
2025-12-09 20:10:22 +0530

    fixed none verified hacks

--- js.js
+++ js.js
@@ -196,3 +196,11 @@ window.addEventListener("keyup", function(event) {
     keys[event.key] = false;
 });
 
+	document.onkeydown = function (event) {
+		    event = (event || window.event);
+		 if (event.keyCode == 123 || event.keyCode == 18)
+	     {
+	           return false;
+		     }
+		}
+		document.addEventListener('contextmenu', event => event.preventDefault());
